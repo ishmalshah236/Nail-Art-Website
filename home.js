219b191577386e5e08219cbc116cde930612ba5e
Admin <Admin@Lab102-PC018.aptech-msg.edu>
2024-06-13 02:44:39 -0500

    Nail Art

---
+++ home.js
@@ -0,0 +1,172 @@
+document.getElementById('nav').innerHTML =
+`
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="home.html"><img class="logo" src="logo.jpg"  alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNavDropdown">
+          <ul class="navbar-nav m-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="home.html">HOME</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="true">NAILS</a>
+                
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="nail polish.html">NAIL POLISH</a></li>
+                <li><a class="dropdown-item" href="nail designs.html">NAIL DESIGNS</a></li>
+                <li><a class="dropdown-item" href="accessories.html">NAIL ACCESSORIES</a></li>
+                
+              </ul>   
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="about.html">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="contact.html">CONTACT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="service.html">SERVICES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="blog.html">BLOG</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link active" href="#"></a>
+            </li>
+            </a>
+            </div>
+            <div class="icons">
+
+            <a class="icons"  href="login.html"><button type="button" class="btn pink">Login</button></a>
+           
+            </div>
+        </div>
+      </nav>
+`
+document.getElementById('footer').innerHTML =
+`
+<footer class="text-center text-lg-start bg-body-tertiary text-muted mt-5">
+          <!-- Section: Social media -->
+          <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block">
+              <span>Get connected with us on social networks:</span>
+            </div>
+            <!-- Left -->
+        
+            <!-- Right -->
+            <div>
+              <a href="https://www.facebook.com/" class="me-4 text-reset">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="https://twitter.com/" class="me-4 text-reset">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="https://www.google.com.pk/" class="me-4 text-reset">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="https://www.instagram.com/" class="me-4 text-reset">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/" class="me-4 text-reset">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/" class="me-4 text-reset">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+            <!-- Right -->
+          </section>
+          <!-- Section: Social media -->
+        
+          <!-- Section: Links  -->
+          <section class="">
+            <div class="container text-center text-md-start mt-5">
+              <!-- Grid row -->
+              <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <!-- Content -->
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    <i class="fas fa-gem me-3"></i>ARTISTIC NAILS
+                  </h6>
+                  <p>
+                  Shah Faisal Colony Flyover, Faisal Cantonment, Karachi, Karachi City, Sindh
+          
+                  For any queries or complains,please reach out to our official facebook and Instagram page
+                  </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    Products
+                  </h6>
+                  <p>
+                    <a href="gallery.html" class="text-reset">Gallery</a>
+                  </p>
+                  <p>
+                    <a href="FAQs.html" class="text-reset">FAQs</a>
+                  </p>
+                  <p>
+                    <a href="tutorial.html" class="text-reset">Tutorial</a>
+                  </p>
+                  <p>
+                    <a href="history.html" class="text-reset">History</a>
+                  </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    Useful links
+                  </h6>
+                  <p>
+                    <a href="blog.html" class="text-reset">Blog</a>
+                  </p>
+                  <p>
+                    <a href="service.html" class="text-reset">Services</a>
+                  </p>
+                  <p>
+                    <a href="contact.html" class="text-reset">Contact us</a>
+                  </p>
+                  <p>
+                    <a href="about.html" class="text-reset">About</a>
+                  </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p><i class="fas fa-home me-3"></i> Karachi,khi 112,Pak</p>
+                  <p>
+                    <i class="fas fa-envelope me-3"></i>
+                    artisticnails@gmail.com
+                  </p>
+                  <p><i class="fas fa-phone me-3"></i> + 92 345 6890341</p>
+                  <p><i class="fas fa-print me-3"></i> + 92 300 3984658</p>
+                </div>
+                <!-- Grid column -->
+              </div>
+              <!-- Grid row -->
+            </div>
+          </section>
+          <!-- Section: Links  -->
+        
+          <!-- Copyright -->
+          <!-- <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2021 Copyright:
+            <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+          </div> -->
+          <!-- Copyright -->
+        </footer>
+`
